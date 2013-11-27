8718f61c09c2b0b4d9e50fb5b935826ee23661b5
Kashka Enverga <kdp.enverga@gmail.com>
2013-11-27 15:35:01 +0800

    basic-screenflow *no menus

--- gen/com/bitizen/counterswipe/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.bitizen.counterswipe;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/com/bitizen/cameratest/FromXML.java
+++
@@ -1,5 +0,0 @@
-package com.bitizen.cameratest;
-
-public @interface FromXML {
-
-}
