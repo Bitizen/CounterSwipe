1274470146eb412a0bd33665dfd1455b411a0151
Kashka Enverga <kdp.enverga@gmail.com>
2013-12-03 19:11:01 +0800

    working client (login)

--- src/com/bitizen/counterswipe/LoginActivity.java
+++ src/com/bitizen/counterswipe/LoginActivity.java
@@ -1,5 +1,12 @@
 package com.bitizen.counterswipe;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Random;
 
 import android.R.layout;
@@ -25,7 +32,13 @@ public class LoginActivity extends Activity implements View.OnClickListener{
 
 	private final Context CONTEXT = this;
 	private final String KEY_USERNAME = "username";
-	
+
+    private Socket socket;
+    private static final int SERVERPORT = 6000;
+    //private static final String SERVER_IP = "10.0.2.15";
+    private static final String SERVER_IP = "10.0.1.106"; // DICE
+    //private static final String SERVER_IP = "10.0.2.2";
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -34,6 +47,8 @@ public class LoginActivity extends Activity implements View.OnClickListener{
 		
 		joinBtn.setOnClickListener(this);
 		hostBtn.setOnClickListener(this);
+
+		new Thread(new ClientThread()).start();
 	}
 
 	private void initializeElements() {
@@ -47,6 +62,19 @@ public class LoginActivity extends Activity implements View.OnClickListener{
 		Intent newIntent;
 		switch (view.getId()) {
 			case R.id.btnJoin:
+				try {
+					String clientUsername = usernameEt.getText().toString();
+					PrintWriter out = new PrintWriter(new BufferedWriter(
+							new OutputStreamWriter(socket.getOutputStream())), true);
+					out.println(clientUsername);
+				} catch (UnknownHostException e) {
+		            e.printStackTrace();
+		        } catch (IOException e) {
+		            e.printStackTrace();
+		        } catch (Exception e) {
+		            e.printStackTrace();
+		        }
+				
 				newIntent = new Intent(CONTEXT, AvailableMatchesActivity.class);
 				newIntent.putExtra(KEY_USERNAME, usernameEt.getText().toString());
 				startActivity(newIntent);
@@ -60,4 +88,18 @@ public class LoginActivity extends Activity implements View.OnClickListener{
 		}
 	}
 
+
+	class ClientThread implements Runnable {
+		@Override
+		public void run() {
+			try {
+				InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+				socket = new Socket(serverAddr, SERVERPORT);
+			} catch (UnknownHostException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
 }
